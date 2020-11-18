4b944e628cb243c282fb4cf203a81a69ae024301
Bella-Goodman <71779559+Bella-Goodman@users.noreply.github.com>
2020-11-18 12:43:16 -0800

    gif

--- sketch.js
+++ sketch.js
@@ -1,5 +1,19 @@
+
+var api = "http://api.giphy.com/v1/gifs/search?";
+//url & path
+var apiKey = "&api_key=dc6zaTOxFJmzC"
+ //public key
+var query = "&q=thanks+obama"
+
 function setup() {
-  createCanvas(400, 400);
+  noCanvas();
+  var url = api + apiKey + query;
+  loadJSON(url, gotData);
+}
+function gotData(giphy) {
+  for (var i = 0; i < giphy.data.length; i++){
+  createImg(giphy.data[i].images.original.url);
+}
 }
 
 function draw() {
