fbfa2be087c7ba177ddebbbeb669873e37404aae
Bella-Goodman <71779559+Bella-Goodman@users.noreply.github.com>
2020-11-18 13:03:13 -0800

    notes

--- sketch.js
+++ sketch.js
@@ -1,21 +1,21 @@
-
 var api = "http://api.giphy.com/v1/gifs/search?";
 //url & path
 var apiKey = "&api_key=dc6zaTOxFJmzC"
- //public key
+//public key
 var query = "&q=thanks+obama"
 
 function setup() {
   noCanvas();
   var url = api + apiKey + query;
+  //grouping to create the url
   loadJSON(url, gotData);
-}
-function gotData(giphy) {
-  for (var i = 0; i < giphy.data.length; i++){
-  createImg(giphy.data[i].images.original.url);
-}
+  //use url with a call back
 }
 
-function draw() {
-  background(220);
+function gotData(giphy) {
+  for (var i = 0; i < giphy.data.length; i++) {
+    //creates an images element for all images pulled from giphy
+    createImg(giphy.data[i].images.original.url);
+    //makes html element with an arguement = source, element that appears on the web page
+  }
 }
